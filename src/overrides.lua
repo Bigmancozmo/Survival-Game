7bbcb23fe045a8b5752dedc30fbe07fa07273fee
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-12-02 22:47:14 -0500

    Update overrides.lua

--- src/overrides.lua
+++ src/overrides.lua
@@ -1,9 +1,9 @@
-local originalFunction = love.graphics.setColor
+local love_graphics_setColor = love.graphics.setColor
 love.graphics.setColor = function(r, g, b, a)
 	r = r or 255
 	g = g or 255
 	b = b or 255
 	a = a or 255
 
-	originalFunction(r / 255, g / 255, b / 255, a / 255)
+	love_graphics_setColor(r / 255, g / 255, b / 255, a / 255)
 end
